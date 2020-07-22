ccaf276475c8e6c68e1654717b3a5cf91ca85e40
Etienne Ghashehbaba <admin@aheadbl.com>
2020-07-22 15:57:17 -0700

    Yes

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var passportLocalMongoose = require('passport-local-mongoose');
+var bcrypt = require('bcrypt');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
@@ -15,6 +16,29 @@ var UserSchema = new Schema({
 });
 
 UserSchema.plugin(passportLocalMongoose);
+
+UserSchema.pre('save', function (next) {
+  var user = this;
+  console.log("saving");
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.genSalt(10, function (err, salt) {
+      if (err) {
+        return next(err);
+      }
+      console.log(salt + "\nwe got da password");
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) {
+          return next(err);
+        }
+        console.log(hash + "\nhashed?");
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
 
 module.exports = mongoose.model('User', UserSchema);
 		
